dc72981f13f0b94e32ff9b5c698724aae3e15953
Suiradkr <dariusroberts12@gmail.com>
2022-12-08 16:21:33 -0500

    added models and migrations

--- server/app_watch/models.py
+++ server/app_watch/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import (AbstractUser)
 # Create your models here.
+
+
+class AppUser(AbstractUser):
+    #User account
+    email = models.EmailField(max_length=255)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    year = models.IntegerField()
+
+class Watched(models.Model):
+    user = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    
+class Watchlist(models.Model):
+    user = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+class Favorite(models.Model):
+    user = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
--- server/server/settings.py
+++ server/server/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'what_to_watch',
     }
 }
 
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'app_watch.AppUser'
