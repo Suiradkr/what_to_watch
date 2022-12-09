6ec0e5d0dc56e67af9108daec158e59f40970dc4
Suiradkr <dariusroberts12@gmail.com>
2022-12-08 20:29:40 -0500

    react and django are connected

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/static/',
+  build: {
+    outDir: '../server/static',
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+  
+  
   plugins: [react()]
 })
